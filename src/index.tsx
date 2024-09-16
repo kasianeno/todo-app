8576b7bbd3ae203514ab201df9acff6a52acccef
Oleksandr <dedikad@gmail.com>
2024-09-16 20:04:19 +0200

    final ver

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,16 @@
 import { createRoot } from 'react-dom/client';
 
-import 'bulma/css/bulma.css';
-import '@fortawesome/fontawesome-free/css/all.css';
-import './index.scss';
+import './styles/index.css';
+import './styles/todo-list.css';
+import './styles/filters.css';
 
 import { App } from './App';
+import { GlobalStateProvider } from './components/management/TodoContext';
 
-createRoot(document.getElementById('root') as HTMLDivElement).render(<App />);
+const container = document.getElementById('root') as HTMLDivElement;
+
+createRoot(container).render(
+  <GlobalStateProvider>
+    <App />
+  </GlobalStateProvider>,
+);
